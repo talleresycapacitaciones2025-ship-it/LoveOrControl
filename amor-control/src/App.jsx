d1d7ac8473de4601ba583d0468c0861af3840878
talleresycapacitaciones2025-ship-it <talleresycapacitaciones2025@gmail.com>
2026-05-08 16:32:33 -0500

    Update App.jsx

--- amor-control/src/App.jsx
+++ amor-control/src/App.jsx
@@ -1,4 +1,6 @@
 import AmorOControlApp from "./AmorOControlApp_VERIFICADO.jsx";
+import { useElevenLabs } from './hooks/useElevenLabs';
+import { AudioControls } from './components/AudioControls';
 
 export default function App() {
   return <AmorOControlApp />;
